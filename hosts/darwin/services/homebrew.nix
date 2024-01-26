12a8349c104f2c731ea15c281cd592345952904d
Lush Sleutsky <lushsleutsky@gmail.com>
2024-01-26 11:00:35 -0500

    revert: homebrew casks

--- hosts/darwin/services/homebrew.nix
+++ hosts/darwin/services/homebrew.nix
@@ -6,9 +6,6 @@
     caskArgs.no_quarantine = true;
     global.autoUpdate = true;
     global.brewfile = true;
-    brews = [
-      "tty-clock"
-    ];
     casks = [
       "1password-nightly"
       "boom-3d"
@@ -16,11 +13,6 @@
       "iterm2-nightly"
       "notunes"
       "smcfancontrol"
-    ];
-    taps = [
-      "homebrew/cask"
-      "homebrew/cask/fonts"
-      "homebrew/cask/versions"
     ];
   };
 }
