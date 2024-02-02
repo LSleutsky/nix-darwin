7625cb532bbe4cca0e2105403036dd8a06d7b8f7
Lush Sleutsky <LushSleutsky@gmail.com>
2024-02-02 13:38:18 -0500

    feat: hyper terminal plugins

--- modules/home-manager/dotfiles/hyper.nix
+++ modules/home-manager/dotfiles/hyper.nix
@@ -4,8 +4,16 @@
     text = ''
       module.exports = {
         config: {
-          fontFamily: 'JetBrainsMono Nerd Font',
-        }
+          fontFamily: 'JetBrainsMono Nerd Font, RobotoMono Nerd Font, monospace',
+          fontSize: 10,
+        },
+        plugins: [
+          "hyper-highlight-active-pane",
+          "hyper-pane",
+          "hyper-search",
+          "hyper-tab-icons",
+          "hypercwd",
+        ]
       };
     '';
   };
