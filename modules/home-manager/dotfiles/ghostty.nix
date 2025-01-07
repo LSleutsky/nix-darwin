894d15787e749eb6c8bc1b88f67df6d4991ff791
LSleutsky <LushSleutsky@gmail.com>
2025-01-06 22:23:08 -0500

    build: ghostty config

--- modules/home-manager/dotfiles/ghostty.nix
+++ modules/home-manager/dotfiles/ghostty.nix
@@ -2,6 +2,7 @@
   xdg.configFile."ghostty/config" = {
     enable = true;
     text = ''
+      confirm-close-surface = false
       font-family = "BerkeleyMono Nerd Font"
       font-size = 10
       theme = catppuccin-mocha
