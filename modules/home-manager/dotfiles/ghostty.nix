9a86838afb2949c3546f7788a9fb51d8889fbdcc
LSleutsky <LushSleutsky@gmail.com>
2025-09-27 22:55:17 -0400

    build: config update 2025-09-27

--- modules/home-manager/dotfiles/ghostty.nix
+++ modules/home-manager/dotfiles/ghostty.nix
@@ -5,7 +5,7 @@
       confirm-close-surface = false
       font-family = "BerkeleyMono Nerd Font"
       font-size = 10
-      theme = catppuccin-mocha
+      theme = /Applications/Ghostty.app/Contents/Resources/ghostty/themes/Catppuccin Mocha
       window-save-state = always
       keybind = shift+enter=text:\n
     '';
