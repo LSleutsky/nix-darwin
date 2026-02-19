c7ab26373c583fd7655ffa3d4148abf2689bb4de
LSleutsky <LushSleutsky@gmail.com>
2026-02-18 23:01:47 -0500

    build: config update 2026-02-18 v19

--- modules/home-manager/packages/nushell.nix
+++ modules/home-manager/packages/nushell.nix
@@ -73,6 +73,7 @@
       $env.ANDROID_HOME = $"($env.HOME)/Library/Android/sdk"
       $env.BUN_INSTALL = $"($env.HOME)/.bun"
       $env.PATH = ($env.PATH | split row (char esep)
+        | prepend "/usr/local/bin"
         | prepend "/etc/profiles/per-user/lush/bin"
         | prepend "/run/current-system/sw/bin"
         | prepend "/nix/var/nix/profiles/default/bin"
