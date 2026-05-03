f8b13c830bf1f65957b482003ea0477e55ec6851
LSleutsky <LushSleutsky@gmail.com>
2026-05-02 20:02:26 -0400

    build: config update 2026-05-02

--- modules/home-manager/packages/zsh.nix
+++ modules/home-manager/packages/zsh.nix
@@ -144,6 +144,10 @@
         export HISTFILESIZE=100000
         export HISTSIZE=100000
         export SAVEHIST=100000
+
+        export HOMEBREW_NO_AUTO_UPDATE=1
+        export HOMEBREW_NO_ANALYTICS=1
+        export HOMEBREW_NO_INSTALL_CLEANUP=1
       '')
 
       (lib.mkOrder 550 ''
