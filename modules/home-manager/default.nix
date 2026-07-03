266ce1f0a5978576d8011e94becbbc489485e702
LSleutsky <LushSleutsky@gmail.com>
2026-07-03 08:35:58 -0400

    build: config update 2026-07-03 v2

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -15,7 +15,6 @@
     ./packages/eza.nix
     ./packages/fzf.nix
     ./packages/git.nix
-    ./packages/nushell.nix
     ./packages/ripgrep.nix
     ./packages/starship.nix
     ./packages/zoxide.nix
--- modules/home-manager/packages/fzf.nix
+++ modules/home-manager/packages/fzf.nix
@@ -5,5 +5,6 @@
     enable = true;
     enableBashIntegration = true;
     enableZshIntegration = true;
+    historyWidget.command = "";
   };
 }
